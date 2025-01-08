e1897394e61348d988c5ef5b7c77da099fa5703e
Yana Sheglova <yana.shheglova.98@bk.ru>
2025-01-08 11:30:10 +0300

    Correct

--- src/ts/domain/Movie.ts
+++ src/ts/domain/Movie.ts
@@ -1,25 +1,16 @@
 import Buyable from "./Buyable";
 
 export default class Movie implements Buyable {
-    readonly id: number;
-    readonly name: string;
-    readonly year: number;
-    readonly country: string;
-    readonly slogan: string;
-    readonly genre: string;
-    readonly time: string;
-    price: number;
-
-    constructor(id: number, name: string, year: number, country: string, slogan: string, genre: string, time: string, price: number) {
-        this.id = id;
-        this.name = name;
-        this.year = year;
-        this.country = country;
-        this.slogan = slogan;
-        this.genre = genre;
-        this.time = time;
-        this.price = price;
-    }
+    constructor(
+        public readonly id: number,
+        public readonly name: string,
+        public readonly year: number,
+        public readonly country: string,
+        public readonly slogan: string,
+        public readonly genre: string,
+        public readonly time: string,
+        public price: number,
+    ) {}
 
     getInfo(): string {
         return `
